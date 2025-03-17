b3d2f005a381cbbcf65501d8a807be1a31f8c17d
Bruno Gomes <23224552+Lodek@users.noreply.github.com>
2025-03-17 14:55:44 -0300

    feat: Add CreatePolicyWithSpecification method (#13)

--- internal/policy/cmd.go
+++ internal/policy/cmd.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sourcenetwork/acp_core/internal/raccoon"
 	"github.com/sourcenetwork/acp_core/internal/zanzi"
 	"github.com/sourcenetwork/acp_core/pkg/auth"
+	"github.com/sourcenetwork/acp_core/pkg/errors"
 	"github.com/sourcenetwork/acp_core/pkg/runtime"
 	"github.com/sourcenetwork/acp_core/pkg/types"
 )
@@ -98,5 +99,86 @@ func HandleDeletePolicy(ctx context.Context, runtime runtime.RuntimeManager, req
 
 	return &types.DeletePolicyResponse{
 		Found: found,
+	}, nil
+}
+
+type CreatePolicyWithSpecHandler struct{}
+
+// Execute consumes the data supplied in the command and creates a new ACP Policy and stores it in the given engine.
+func (c *CreatePolicyWithSpecHandler) Execute(ctx context.Context, runtime runtime.RuntimeManager, req *types.CreatePolicyWithSpecificationRequest) (*types.CreatePolicyWithSpecificationResponse, error) {
+	engine, err := zanzi.NewZanzi(runtime.GetKVStore(), runtime.GetLogger())
+	if err != nil {
+		return nil, err
+	}
+
+	counter := raccoon.NewCounterStoreFromRuntimeManager(runtime, policyCounterPrefix)
+	releaser := counter.Acquire()
+	defer releaser.Release()
+	i, err := counter.GetNextAndIncrement(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("CreatePolicy: %w", err)
+	}
+
+	now, err := runtime.GetTimeService().GetNow(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	principal, err := auth.ExtractPrincipal(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	policy, err := Unmarshal(req.Policy, req.MarshalType)
+	if err != nil {
+		return nil, fmt.Errorf("CreatePolicy: %w", err)
+	}
+
+	// if no spec was provided, assign the required spec
+	if policy.SpecificationType == types.PolicySpecificationType_UNKNOWN_SPEC {
+		policy.SpecificationType = req.RequiredSpec
+	}
+
+	if policy.SpecificationType != req.RequiredSpec {
+		return nil, errors.Wrap("CreatePolicy: invalid specification type", errors.ErrorType_BAD_INPUT,
+			errors.Pair("expected", req.RequiredSpec),
+			errors.Pair("got", policy.SpecificationType),
+		)
+	}
+
+	pipeline := ppp.PipelineFactory(i, req.RequiredSpec)
+	policy, err = pipeline.Process(policy)
+	if err != nil {
+		return nil, fmt.Errorf("CreatePolicy: %w", err)
+	}
+
+	record := &types.PolicyRecord{
+		Policy:           policy,
+		PolicyDefinition: req.Policy,
+		MarshalType:      req.MarshalType,
+		Metadata: &types.RecordMetadata{
+			Creator:    &principal,
+			CreationTs: now,
+			Supplied:   req.Metadata,
+		},
+	}
+
+	err = engine.SetPolicy(ctx, record)
+	if err != nil {
+		return nil, fmt.Errorf("CreatePolicy: %w", err)
+	}
+
+	eventManager := runtime.GetEventManager()
+	event := types.EventPolicyCreated{
+		PolicyId:   record.Policy.Id,
+		PolicyName: record.Policy.Name,
+	}
+	err = eventManager.EmitEvent(&event)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.CreatePolicyWithSpecificationResponse{
+		Record: record,
 	}, nil
 }
