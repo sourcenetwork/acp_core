08e911233bae9f8398548ed90c8c7638426641d4
Bruno Gomes <gomes.bruno.ac@gmail.com>
2025-08-12 14:53:02 -0300

    feat: Return parsed Policy from Validate

--- test/integration/engine/policy/validate.go
+++ test/integration/engine/policy/validate.go
@@ -55,3 +55,113 @@ resources:
 	require.False(t, resp.Valid)
 	require.Contains(t, resp.ErrorMsg, "defra policy specification")
 }
+
+func TestValidatePolicy_ReturnsParsedPolicy(t *testing.T) {
+	ctx := test.NewTestCtx(t)
+
+	policyStr := `
+name: policy
+description: ok
+resources:
+  file:
+    relations:
+      owner:
+        doc: owner owns
+        types:
+          - actor-resource
+      reader:
+      admin:
+        manages:
+          - reader
+    permissions:
+      own:
+        expr: owner
+        doc: own doc
+      read:
+        expr: owner + reader
+
+meta:
+  a: b
+  key: value
+
+actor:
+  name: actor-resource
+  doc: my actor
+`
+	msg := types.ValidatePolicyRequest{
+		Policy:      policyStr,
+		MarshalType: types.PolicyMarshalingType_SHORT_YAML,
+	}
+	resp, err := ctx.Engine.ValidatePolicy(ctx, &msg)
+
+	require.Nil(t, err)
+	require.Equal(t, &types.Policy{
+		Id:                "da7be65027664708551f97197ba5f5993aa99bc7b57055df9766426dc6da9605",
+		Name:              "policy",
+		Description:       "ok",
+		SpecificationType: types.PolicySpecificationType_NO_SPEC,
+		Attributes: map[string]string{
+			"a":   "b",
+			"key": "value",
+		},
+		Resources: []*types.Resource{
+			{
+				Name: "file",
+				Relations: []*types.Relation{
+					{
+						Name: "admin",
+						Manages: []string{
+							"reader",
+						},
+						VrTypes: []*types.Restriction{},
+					},
+					{
+						Name: "owner",
+						Doc:  "owner owns",
+						VrTypes: []*types.Restriction{
+							{
+								ResourceName: "actor-resource",
+								RelationName: "",
+							},
+						},
+					},
+					{
+						Name: "reader",
+					},
+				},
+				Permissions: []*types.Permission{
+					{
+						Name:       "_can_manage_admin",
+						Expression: "owner",
+						Doc:        "permission controls actors which are allowed to create relationships for the admin relation (permission was auto-generated).",
+					},
+					{
+						Name:       "_can_manage_owner",
+						Expression: "owner",
+						Doc:        "permission controls actors which are allowed to create relationships for the owner relation (permission was auto-generated).",
+					},
+					{
+						Name:       "_can_manage_reader",
+						Expression: "(admin + owner)",
+						Doc:        "permission controls actors which are allowed to create relationships for the reader relation (permission was auto-generated).",
+					},
+					{
+						Name:       "own",
+						Expression: "owner",
+						Doc:        "own doc",
+					},
+					{
+						Name:       "read",
+						Expression: "(owner + reader)",
+					},
+				},
+			},
+		},
+		ActorResource: &types.ActorResource{
+			Name: "actor-resource",
+			Doc:  "my actor",
+		},
+	},
+		resp.Policy,
+	)
+}
