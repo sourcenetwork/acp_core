1bc7a9432ecb3557cb9be07e62e66131af854b97
Bruno Gomes <gomes.bruno.ac@gmail.com>
2026-05-14 16:07:49 -0300

    pb gen

--- pkg/types/relationship.pb.go
+++ pkg/types/relationship.pb.go
@@ -218,6 +218,7 @@ var xxx_messageInfo_AllActors proto.InternalMessageInfo
 // Subject specifies the target of a Relationship.
 type Subject struct {
 	// Types that are valid to be assigned to Subject:
+	//
 	//	*Subject_Actor
 	//	*Subject_ActorSet
 	//	*Subject_AllActors
